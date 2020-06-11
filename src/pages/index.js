e7d1182a356d2280dca3ac0d557193af0fc5ffa5
Samuel Howard <samuelhowardstudent@gmail.com>
2020-06-11 19:18:37 -0400

    Edited home page to reflect new Vidi interpreter

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,13 +19,13 @@ const IndexPage = () => (
 	</div>
 	<h2>Current state of Vidi</h2>
 	<p>
-	The current version of Vidi is evaluated using an interpreter built in OCaml. It is being solely developed by
+	The current version of Vidi is evaluated using an interpreter built in C++. Previously, it was developed using OCaml. It is being solely developed by
     {` `}
 	<a style={{ color: `#46b4eb` }} href="https://github.com/SamuelHoward">Samuel Howard</a>. Vidi supports fundamental data types, arrays, functions, and command line arguments. Precise descriptions of the language's features can be seen in the documentation and are demonstrated in the samples. 
 	</p>
 	<h2>Goals for the Language</h2>
 	<p>
-The current primary goal for the language is to make it open source via GitHub. The reason this has not been done is because the sole interpreter for the language is written in OCaml. Since the language's targeted audience is novice programmers, it would be preferable for the interpreter to written in a more common language, so that even relatively inexperienced programmers could contribute to the language. The plan is for Vidi to go open source after such an interpreter is implemented.
+The current primary goal for the language is to make it open source via GitHub. The reason this has not been done is because the previous interpreter for the language was written in OCaml. The new interpreter is being developed in C++. The change was made because C++ is used more frequently than OCaml, so it should be more possible for relatively inexperienced programmers to contribute to the language. The plan is for Vidi to go open source after such an interpreter is implemented.
 	</p>
 	<p>
 	Aside from that, some general goals of the language are to ship with a debugger, as well as include support for creating advanced and linked data structures.
